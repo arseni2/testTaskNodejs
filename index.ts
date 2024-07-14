a341ce4d8fa4e67410db711f5aac142ca62d51be
arseni2 <arc-37@mail.ru>
2024-07-14 21:04:08 +0300

    better perfomance

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-import {ICharacter, IResponse} from "./types";
+import { ICharacter, IResponse } from "./types";
 import * as fs from "node:fs";
-import {Client} from "pg";
+import { Client } from "pg";
 
 const config = {
 	connectionString:
@@ -19,68 +19,69 @@ const conn = new Client(config);
 conn.connect((err) => {
 	if (err) throw err;
 });
+
 const createTable = async () => {
 	await conn.query(`CREATE TABLE IF NOT EXISTS ${tableName} (
-			id SERIAL PRIMARY KEY,
-			name TEXT,
-			data JSONB
-		);
-                `)
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        data JSONB
+    );`);
 	console.log("Table created successfully");
-}
-
+};
 
 const fetchCharacters = async (nextPageUrl: string): Promise<IResponse> => {
 	try {
-		console.log(`START REQUEST URL = ${nextPageUrl}`)
+		console.log(`START REQUEST URL = ${nextPageUrl}`);
 		const response = await axios.get(nextPageUrl);
 		return response.data;
 	} catch (error) {
 		console.error("Error fetching characters:", error);
 		throw error;
 	}
-}
+};
 
 const getAllCharacter = async (): Promise<ICharacter[]> => {
 	let characters = [];
 	let nextPageUrl = "https://rickandmortyapi.com/api/character?page=1";
 	while (nextPageUrl) {
-		const response = await fetchCharacters(nextPageUrl)
-		nextPageUrl = response.info.next as string
-		characters.push(...response.results)
+		const response = await fetchCharacters(nextPageUrl);
+		nextPageUrl = response.info.next as string;
+		characters.push(...response.results);
 	}
-
 	return characters;
-}
+};
+
+const insertAllCharacters = async (characters: ICharacter[]) => {
+	const values = characters
+		.map((char) => `('${char.name.replace(/'/g, "''")}', '${JSON.stringify(char).replace(/'/g, "''")}')`)
+		.join(",");
+	const insertQuery = `
+        INSERT INTO ${tableName} (name, data)
+        VALUES ${values}
+        RETURNING *;
+    `;
+	try {
+		await conn.query(insertQuery);
+		console.log("All characters inserted successfully");
+	} catch (error) {
+		console.error("Ошибка при вставке всех персонажей:", error);
+		throw error;
+	}
+};
+
 createTable()
 	.catch((err) => {
-		console.log("Не удалось создать таблицу")
+		console.log("Не удалось создать таблицу", err);
 	})
 	.then(() => {
 		getAllCharacter()
-			.then(async characters => {
-				const insertQuery = `
-						INSERT INTO ${tableName} (name, data)
-						VALUES ($1, $2)
-						RETURNING *;
-        			`;
-				const inserted = characters.map((item, i) => {
-					console.log("Вставка персонажа", item.name)
-					return conn.query(insertQuery, [item.name, JSON.stringify(item)]);
-				})
-				try {
-					await Promise.all(inserted);
-					console.log("Все персонажи успешно вставлены");
-					await conn.end()
-				} catch (error) {
-					console.error("Ошибка при вставке персонажей:", error);
-					await conn.end()
-					throw error;
-				}
+			.then(async (characters) => {
+				await insertAllCharacters(characters);
+				console.log("Все персонажи успешно вставлены");
+				await conn.end();
 			})
-			.catch(async error => {
+			.catch(async (error) => {
 				console.error('Ошибка при получении персонажей:', error);
-				await conn.end()
+				await conn.end();
 			});
-	})
-
+	});
